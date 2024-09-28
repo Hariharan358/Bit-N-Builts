296068bfdb64da3fae6170f5709a7a618f7a5dea
Akash1912-hub <231401004@rajalakshmi.edu.in>
2024-09-29 04:38:38 +0530

    Create ADMIN.sol

---
+++ ADMIN.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleLogin {
+    function login(string memory, string memory) public pure returns (bool) {
+        // Always return true for demonstration purposes
+        return true;
+    }
+}
